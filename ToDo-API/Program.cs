499f1e1373b5f07bb83af4537e68b557552e1178
Binjan Iyer <binjan77@gmail.com>
2022-02-28 15:15:45 +0530

    commit

--- ToDo-API/Program.cs
+++ ToDo-API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ToDo_API
@@ -13,6 +14,10 @@ namespace ToDo_API
     {
         public static void Main(string[] args)
         {
+            // Binjan; v1.9.1.0; 201/10/04; this line is newly added to solve issue to download images
+            // e.g. http://www.rightmove.co.uk/overseas-property/find/Beverywherecom/UK.html?locationIdentifier=BRANCH%5E88684
+            ServicePointManager.SecurityProtocol =                                          SecurityProtocolType.Tls13;
+
             CreateHostBuilder(args).Build().Run();
         }
 
